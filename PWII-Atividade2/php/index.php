2b98c4e36e7ee6bb44be16e8d15eb95eae9a207e
MuriloOtavio <murilo.dias19@etec.sp.gov.br>
2025-05-03 21:37:30 -0300

    Update index.php

--- PWII-Atividade2/php/index.php
+++ PWII-Atividade2/php/index.php
@@ -1,5 +1,4 @@
 <?php
-// Conexão básica com o banco (opcional)
 $host = "localhost";
 $user = "root";
 $pass = "";
@@ -10,26 +9,3 @@ if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Home</title>
-</head>
-<body>
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="produtos.php">Produtos</a>
-        <a href="cadastro.php">Cadastro</a>
-        <a href="login.php">Login</a>
-        <a href="contato.php">Contato</a>
-    </nav>
-
-    <h1>Bem-vindo!</h1>
-    <?php
-    // Exemplo simples de PHP
-    date_default_timezone_set('America/Sao_Paulo');
-    echo "<p>Hoje é: " . date('d/m/Y H:i') . "</p>";
-    ?>
-</body>
-</html>
