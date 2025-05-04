60bb37aa632fdb80404cf44b26a6da264f2c1cb6
MuriloOtavio <murilo.dias19@etec.sp.gov.br>
2025-05-03 21:37:03 -0300

    Update contato.php

--- PWII-Atividade2/php/contato.php
+++ PWII-Atividade2/php/contato.php
@@ -1,10 +1,9 @@
 <?php
-// Processar contato
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nome = htmlspecialchars($_POST['nome']);
     $mensagem = htmlspecialchars($_POST['mensagem']);
     
-    // Simulação de envio
     $arquivo = fopen("contatos.txt", "a");
     fwrite($arquivo, "{$nome}: {$mensagem}\n");
     fclose($arquivo);
@@ -12,23 +11,3 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $sucesso = true;
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Contato</title>
-</head>
-<body>
-    <nav><!-- Menu igual ao index --></nav>
-
-    <h1>Contato</h1>
-    
-    <?php if (isset($sucesso)) echo "<p>Mensagem enviada!</p>"; ?>
-    
-    <form method="POST">
-        Nome: <input type="text" name="nome" required><br>
-        Mensagem: <textarea name="mensagem" required></textarea><br>
-        <button type="submit">Enviar</button>
-    </form>
-</body>
-</html>
